732a69954ed8a8948e7551b59c0b29c4d85ca54b
satomi <satomi3813281@gmail.com>
2021-10-31 19:29:09 +0900

    お問い合わせフォームエラー表示について

--- src/main/java/com/example/inagakipg/MyController.java
+++ src/main/java/com/example/inagakipg/MyController.java
@@ -2,6 +2,7 @@ package com.example.inagakipg;
 
 
 import org.springframework.boot.autoconfigure.batch.BatchProperties;
+import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.web.bind.annotation.*;
 import java.applet.Applet;
 
@@ -13,11 +14,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.xml.namespace.QName;
 import java.util.List;
 import java.util.Map;
 
 @Controller
 public class MyController {
+//    jdbcTemplate変数、 @AutowiredあるおかげでjdbcTemplate変数にインスタンスが入る
+//    JdbcTemplate jdbcTemplate = new JdbcTemplate();
     @Autowired
     JdbcTemplate jdbcTemplate;
 
@@ -36,17 +40,54 @@ public class MyController {
 
     @PostMapping("/form/result")
     public String getFormResult(@ModelAttribute ContactForm form, Model model) {
-        model.addAttribute("form", form);
-        jdbcTemplate.update("INSERT INTO contactdata(name,email,message) Values(?,?,?)", form.getName(), form.getEmail(), form.getMessage());
-        return "result";
+        if (name.isEmpty()) {
+            model.addAttribute("coment", "名前を入力してください");
+            return "contactError";
+        } else if (email.isEmpty()) {
+            model.addAttribute("coment", "アドレスをを入力してください");
+            return "contactError";
+        } else if (message.isEmpty()) {
+            model.addAttribute("coment", "内容をを入力してください");
+            return "contactError";
+        } else {
+            model.addAttribute("form", form);
+            jdbcTemplate.update("INSERT INTO contactdata(name,email,message) Values(?,?,?)", form.getName(), form.getEmail(), form.getMessage());
+            return "result";
+        }
+
     }
+
+
 
 
     @GetMapping("/form/list")
     public String getFormList(Model model) {
         List<Map<String, Object>> contacts = jdbcTemplate.queryForList("select * from contactdata");
-        model.addAttribute("contacts", contacts);
+        model.addAttribute("contactsData", contacts);
         return "list";
+    }
+
+//    blog
+@GetMapping("/blogForm")
+public String getBlogForm(Model model) {
+    model.addAttribute("blogForm", new blog());
+    return "blogForm";
+}
+
+
+    @PostMapping("/blogForm/blogResult")
+    public String getBlogFormResult(@ModelAttribute blog blogForm, Model model) {
+        model.addAttribute("blogForm", blogForm);
+        jdbcTemplate.update("INSERT INTO blogdata(thema,matter) Values(?,?)",blogForm.getThema(), blogForm.getMatter());
+        return "blogResult";
+    }
+
+
+    @GetMapping("/blogForm/blogList")
+    public String getBlogFormList(Model model) {
+        List<Map<String, Object>> blog = jdbcTemplate.queryForList("select * from blogdata");
+        model.addAttribute("BlogContentData", blog);
+        return "blogList";
     }
 
 }
--- src/main/java/com/example/inagakipg/blog.java
+++ src/main/java/com/example/inagakipg/blog.java
@@ -1,5 +1,23 @@
 package com.example.inagakipg;
 
 public class blog {
+    private String thema;
+    private String matter;
+
+
+    public String getThema() {
+        return thema;
+    }
+    public void setThema(String thema) {
+        this.thema = thema;
+    }
+
+    public String getMatter() {
+        return matter;
+    }
+    public void setMatter(String matter) {
+        this.matter = matter;
+    }
+
 
 }
